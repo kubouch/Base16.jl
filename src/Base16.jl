a10d741d3c6b82659ecf649153adb2a0c13b92ef
Jakub Žádník <kubouch@gmail.com>
2022-01-26 23:54:22 +0200

    Refactor main() into multiple functions

--- src/Base16.jl
+++ src/Base16.jl
@@ -9,10 +9,11 @@ using .KMeans
 
 const MIN_BRIGHTNESS = 0.1
 const MAX_BRIGHTNESS = 0.85
-const SHOW_PLOT = true
-const SHOW_COLORS = true
+const SHOW_PLOT = false
+const SHOW_COLORS = false
 const NITER = 25
 const COLOR_SPACE = "lab"
+const TEST_IMG = "/home/kubouch/pictures/kodim/raw/kodim23.png"
 
 function select_base_colors(
     img::Base.ReinterpretArray{T,3,Lab{T}}
@@ -71,103 +72,68 @@ function select_base_colors(
     return bases
 end
 
-function main()
-    # img = load("/home/kubouch/pictures/mini/firewatch_4x4.png")
-    # img = load("/home/kubouch/pictures/mini/kodim23_crop_24x24a.png")
-    img = load("/home/kubouch/pictures/kodim/raw/kodim23.png")
-    # img = load("/home/kubouch/pictures/kodim/raw/kodim13.png")
-    # img = load("/tmp/wallpaper.jpg")
+function show_colors(means_rgb)
+    display(colorview(RGB, means_rgb))
+end
 
-    color_space = if COLOR_SPACE == "lab"
-        Lab
-    elseif COLOR_SPACE == "rgb"
-        RGB
+function show_plot(img, bases, means)
+    # Plotly backend opens a browser at the end
+    plotly()
+
+    # Plot the image pixels
+    # Limit max. number of plotted pixels, otherwise it's too slow
+    sz = (min(100, size(img)[2]), min(100, size(img)[2]))
+    plot_img = channelview(sample(img, sz))
+
+    scatter(
+        plot_img[1, :, :],
+        plot_img[2, :, :],
+        plot_img[3, :, :];
+        # Too slow:
+        # marker_z=plot_img[3, :, :],
+        markersize=0.5,
+        markerstrokewidth=0,
+        markeralpha=0.5,
+        markercolor="black",
+        legend=false,
+        size=(1000, 1000),
+    )
+
+    # Plot the fixed base colors first
+    nbases = if ndims(bases) == 1
+        size(bases)[1]
     else
-        error("Unknown color space: ", COLOR_SPACE)
+        size(bases)[2]
     end
 
-    img = color_space.(img)
+    scatter!(
+        means[1, 1:nbases],
+        means[2, 1:nbases],
+        means[3, 1:nbases];
+        # marker_z=means[3, :, :],
+        markersize=2,
+        # markerstrokewidth=0,
+        markercolor="blue",
+        legend=false,
+        size=(1000, 1000),
+    )
 
-    # Image types other than RGB do not support math ops => need raw pixels
-    inp_img = channelview(img)
-
-    bases = select_base_colors(inp_img)
-
-    means = KMeans.kmeans(inp_img, bases, 8, NITER)
-
-    # Convert the result back to RGB
-    means_rgb = RGB.(colorview(color_space, means))
-    # means_rgb = zeros(size(means))
-
-    # for i in 1:size(means_rgb)[2]
-    #     rgb = RGB(means[:, i]...)
-    #     means_rgb[1, i] = rgb.r
-    #     means_rgb[2, i] = rgb.g
-    #     means_rgb[3, i] = rgb.b
-    # end
-
-    if SHOW_COLORS
-        display(colorview(RGB, means_rgb))
-    end
-
-    if SHOW_PLOT
-        # Plotly backend opens a browser at the end
-        plotly()
-
-        # Plot the image pixels
-        # Limit max. number of plotted pixels, otherwise it's too slow
-        sz = (min(100, size(img)[2]), min(100, size(img)[2]))
-        plot_img = channelview(sample(img, sz))
-
-        scatter(
-            plot_img[1, :, :],
-            plot_img[2, :, :],
-            plot_img[3, :, :];
-            # Too slow:
-            # marker_z=plot_img[3, :, :],
-            markersize=0.5,
-            markerstrokewidth=0,
-            markeralpha=0.5,
-            markercolor="black",
-            legend=false,
-            size=(1000, 1000),
-        )
-
-        # Plot the fixed base colors first
-        nbases = if ndims(bases) == 1
-            size(bases)[1]
-        else
-            size(bases)[2]
-        end
-
+    # Plot the rest of the base colors and show the plot
+    gui(
         scatter!(
-            means[1, 1:nbases],
-            means[2, 1:nbases],
-            means[3, 1:nbases];
+            means[1, (nbases + 1):end],
+            means[2, (nbases + 1):end],
+            means[3, (nbases + 1):end];
             # marker_z=means[3, :, :],
             markersize=2,
             # markerstrokewidth=0,
-            markercolor="blue",
             legend=false,
             size=(1000, 1000),
-        )
+        ),
+    )
+end
 
-        # Plot the rest of the base colors and show the plot
-        gui(
-            scatter!(
-                means[1, (nbases + 1):end],
-                means[2, (nbases + 1):end],
-                means[3, (nbases + 1):end];
-                # marker_z=means[3, :, :],
-                markersize=2,
-                # markerstrokewidth=0,
-                legend=false,
-                size=(1000, 1000),
-            ),
-        )
-    end
-
-    # Print out the final colors
+function print_colors(means_rgb)
     for i in 1:length(means_rgb)
         col = means_rgb[i]
 
@@ -177,6 +143,52 @@ function main()
 
         @printf("#%02x%02x%02x\n", r, g, b)
     end
+end
+
+function main()
+
+    prog = Base.basename(Base.source_path())
+    img_name = if length(ARGS) == 0
+        TEST_IMG
+    else
+        ARGS[1]
+    end
+
+    img = load(img_name)
+
+    color_space = if COLOR_SPACE == "lab"
+        Lab
+    elseif COLOR_SPACE == "rgb"
+        RGB
+    else
+        error("Unknown color space: ", COLOR_SPACE)
+    end
+
+    # Color transform
+    img = color_space.(img)
+
+    # Image types other than RGB do not support math ops => need raw pixels
+    inp_img = channelview(img)
+
+    # Select base colors
+    bases = select_base_colors(inp_img)
+
+    # Calculate k-means
+    means = KMeans.kmeans(inp_img, bases, 8, NITER)
+
+    # Convert the result back to RGB
+    means_rgb = RGB.(colorview(color_space, means))
+
+    if SHOW_COLORS
+        show_colors(means_rgb)
+    end
+
+    if SHOW_PLOT
+        show_plot(img, bases, means)
+    end
+
+    # Print out the final colors
+    print_colors(means_rgb)
 
     return nothing
 end
